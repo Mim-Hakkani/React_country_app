bba91c8b285f006218624abc52671ae7307cf096
32-Mimhakkani <mim.cse32@gmail.com>
2022-06-07 12:36:40 +0600

    filter country is workings

--- src/components/Countriesall/Countries.js
+++ src/components/Countriesall/Countries.js
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Country from './Country';
+const { v4: uuidv4 } = require('uuid');
 
 const Countries = () => {
-    
+ 
+    const [country,setCountry] =useState([])
+    const [loading,setLoading] =useState(true)
+
+    useEffect(()=>{
+        //set up the country api 
+
+         fetch('https://restcountries.com/v3.1/all')
+         .then(res=>res.json())
+         .then(data=>
+            // console.log(data),
+             setCountry(data),
+             setLoading(false)
+         )
+    },[])
+
+    // console.log({country,id:uuidv4()}) 
+
+    if(loading) return <img src="/loading.gif" alt="loading" />
 
     const handleSearch =(e)=>{
         e.preventDefault();
 
         console.log(e.target.value);
     
+    }
+
+    const removecountry =(name)=>{
+       const filteritem = country?.filter(filteritem=> filteritem?.name?.common!==name);
+
+       setCountry(filteritem);
+
     }
     return (
         <div>
@@ -30,15 +56,21 @@ const Countries = () => {
                 </div>
             </div>
 
-
-            <div className="country-body">
-                <div className="container">
+{
+    !loading &&  
+     <div className="country-body">
+                 <div className="container">
                     <div className="row"> 
-                         <Country />
+                    {
+                        country?.map((item,index)=> <Country key={index} item={item} removecountry={removecountry}/>)
+                    }
+                        
                     </div>
                     
                 </div>
-            </div>
+    </div>
+}
+          
         </div>
     );
 };
--- src/components/Countriesall/Country.js
+++ src/components/Countriesall/Country.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({item,removecountry}) => {
     return (
                <div className="col-md-4">
                             <div className="card" style={{width: '18rem'}}>
-                                <img src="..." className="card-img-top" alt="..."/>
+                                <img src={item?.flags?.png} className="card-img-top" alt="..." width="100px" height="150px"/>
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                      <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">An item</li>
-                                        <li class="list-group-item">A second item</li>
-                                        <li class="list-group-item">A third item</li>
+                                    <h5 className="card-title">{item?.name?.common}</h5>
+                            
+                                      <ul className="list-group list-group-flush">
+                                   
+                                        <li className="list-group-item">Population:{item?.population}</li>
+                                        <li className="list-group-item">{item?.capital}</li>
                                     </ul>
-                                    <a href="/" className="btn btn-info mt-2 d-block text-white">Go somewhere</a>
+                                    <button 
+                                    onClick={()=>removecountry(item?.name?.common)}
+                                    className="btn btn-info mt-2 d-block text-white">Remove Country</button>
                                 </div>
                                 </div>
                         </div>
